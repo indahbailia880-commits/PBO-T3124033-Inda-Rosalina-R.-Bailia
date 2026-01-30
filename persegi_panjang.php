e5ac12c47f49558d5d85bf0c99c60140dea3b9f0
unknown <indahbailia880@gmail.com>
2026-01-30 21:40:32 +1400

    latihan kedua persegi panjang

--- persegi_panjang.php
+++ persegi_panjang.php
@@ -1,40 +1,37 @@
 <?php
 class PersegiPanjang
 {
+
     public $panjang;
     public $lebar;
 
-    public function hitungLuas()
+    public function hitungluas()
     {
         return $this->panjang * $this->lebar;
     }
 
-    public function hitungKeliling()
+    public function hitungkeliling()
     {
         return 2 * ($this->panjang + $this->lebar);
     }
 }
 
-// objek ke 1
 $pp1 = new PersegiPanjang();
 $pp1->panjang = 10;
 $pp1->lebar = 5;
 
-// objek ke 2
 $pp2 = new PersegiPanjang();
-$pp2->panjang = 7;
-$pp2->lebar = 3;
+$pp2->panjang = 10;
+$pp2->lebar = 5;
 
-// cetak Persegi Panjang 1
 echo "<h3>Persegi Panjang 1</h3>";
-echo "Panjang : " . $pp1->panjang . "<br>";
-echo "Lebar : " . $pp1->lebar . "<br>";
-echo "Luas : " . $pp1->hitungLuas() . "<br>";
-echo "Keliling : " . $pp1->hitungKeliling() . "<br>";
+echo "panjang: " . $pp1->panjang . "<br>";
+echo "lebar: " . $pp1->lebar . "<br>";
+echo "Luas: " . $pp1->hitungluas() . "<br>";
+echo "Keliling: " . $pp1->hitungkeliling() . "<br>";
 
-// cetak Persegi Panjang 2
 echo "<h3>Persegi Panjang 2</h3>";
-echo "Panjang : " . $pp2->panjang . "<br>";
-echo "Lebar : " . $pp2->lebar . "<br>";
-echo "Luas : " . $pp2->hitungLuas() . "<br>";
-echo "Keliling : " . $pp2->hitungKeliling() . "<br>";
+echo "panjang: " . $pp2->panjang . "<br>";
+echo "lebar: " . $pp2->lebar . "<br>";
+echo "Luas: " . $pp2->hitungluas() . "<br>";
+echo "Keliling: " . $pp2->hitungkeliling() . "<br>";
